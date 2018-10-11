4092adbf21d3f1496e0bf38c5d08861983c7aa3c
Junior Suarez <jsuarez92@gmail.com>
2018-10-10 20:02:59 -0400

    Simplificacion en verificacion de promedio fuera de rango

--- promedioCalificaciones.py
+++ promedioCalificaciones.py
@@ -10,25 +10,26 @@ cal4 = int(input("Ingrese la cuarta calificacion: "))
 calificacion = str('');
 
 # Se evalua que todas las cantidades de calificaciones, esten entre 0 y 100
-if (cal1 > 0 and cal1 < 101) and (cal2 > 0 and cal2 < 101) and (cal3 > 0 and cal3 < 101) and (cal4 > 0 and cal4 < 101):
     # Se suman todas las calificaciones y se dividen en cuatro para sacar el promedio
-    suma = cal1+cal2+cal3+cal4
-    promedio = suma//4
-    # Se evalua cual fue el promedio en base a la cantidad se asiga una valor a la variable calificacion
-    if promedio > 89:
-        calificacion = 'A'
-    elif promedio > 79:
-        calificacion = 'B'
-    elif promedio > 74:
-        calificacion = 'C'
-    elif promedio > 69:
-        calificacion = 'D'
-    elif promedio > 59:
-        calificacion = 'FE'
-    elif promedio > 50:
-        calificacion = 'FI'
-    else: 
-        calificacion = 'F' 
+suma = cal1+cal2+cal3+cal4
+promedio = suma//4
+# Se evalua cual fue el promedio en base a la cantidad se asiga una valor a la variable calificacion
+if promedio > 89:
+    calificacion = 'A'
+elif promedio > 79:
+    calificacion = 'B'
+elif promedio > 74:
+    calificacion = 'C'
+elif promedio > 69:
+    calificacion = 'D'
+elif promedio > 59:
+    calificacion = 'FE'
+elif promedio > 50:
+    calificacion = 'FI'
+else: 
+    calificacion = 'F' 
+
+if (promedio > 0 and promedio < 101):
     print("El promedio del estudiante "+nombre+" es: "+str(promedio)+" para una calificacion: "+str(calificacion))
 else:
     print("Hay una calificacion fuera del rango permitido, por favor revise e intente nuevamente.")
